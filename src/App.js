30f633a191b5486581a222d34647829644b34bdc
Matias Fiordelli <>
2022-11-27 11:59:10 -0300

    Starting version 2.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import Formsearch from './Componentes/Formsearch';
-	
+import React, { useState } from 'react';
+import Routess from './Components/Router/Routess'
+import Layout from './Layout'
+import { LanguageContext } from "./GlobalState/context.js"
+
 export default function App() {
-  return (
-	<>
-		<Formsearch/>
-	</>
-  );
+	const initialLanguage = localStorage.getItem('lang') === null
+																?'en-US'
+																:localStorage.getItem('lang')
+	const [languageCodeState, setLanguageCodeState] = useState(initialLanguage)
+
+	return (
+		<LanguageContext.Provider value={{languageCodeState, setLanguageCodeState}}>
+			<Layout>
+				<Routess />
+			</Layout>
+		</LanguageContext.Provider>
+	);
 }
 
 
